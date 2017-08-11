766510623a4f799568e36c2f4f50379173dc6d82
Deepu Shaji <deepushaji4@gmail.com>
2017-08-11 22:12:40 +0530

    repair

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,13 +19,7 @@ class ApplicationController < ActionController::Base
   # text/plain.
 
   def cors_preflight_check
-    if request.method == 'OPTIONS'
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
+
   end
 
   def create_user
