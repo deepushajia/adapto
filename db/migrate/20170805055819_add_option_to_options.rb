6032ce12d05e8fede8aa19809a49f6ef00b62aa5
Deepu Shaji <deepushaji4@gmail.com>
2017-08-11 14:20:53 +0530

    cap change

--- db/migrate/20170805055819_add_option_to_options.rb
+++
@@ -1,5 +0,0 @@
-class AddOptionToOptions < ActiveRecord::Migration[5.0]
-  def change
-    add_column :options, :option, :text
-  end
-end
--- db/migrate/20170805084153_add_topic_id_to_attempt_records.rb
+++
@@ -1,5 +0,0 @@
-class AddTopicIdToAttemptRecords < ActiveRecord::Migration[5.0]
-  def change
-    add_column :attempt_records, :topic_id, :integer
-  end
-end
