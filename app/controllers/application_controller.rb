fda88f82face301c943c7a1bd6281c8984ffb1c7
Deepu Shaji <deepushaji4@gmail.com>
2017-08-11 23:26:08 +0530

    cors issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
 
   def cors_preflight_check
     if request.method == 'OPTIONS'
-      headers['Access-Control-Allow-Origin'] = 'http://139.59.77.222/'
+      headers['Access-Control-Allow-Origin'] = 'http://example.com'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
       headers['Access-Control-Max-Age'] = '1728000'
