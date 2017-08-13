709480f0ccadd0a8f9349b1e8ae9a3eab177a427
Deepu Shaji <deepushaji4@gmail.com>
2017-08-13 14:53:28 +0530

    adding some ajax

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806054645) do
+ActiveRecord::Schema.define(version: 20170811155540) do
 
   create_table "attempt_records", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
